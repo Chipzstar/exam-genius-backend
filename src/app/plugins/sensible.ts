d116f59df8e76d66f887ae30188b0e128ce9830f
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-25 17:01:25 +0100

    init project

--- src/app/plugins/sensible.ts
+++ src/app/plugins/sensible.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 import sensible from '@fastify/sensible';
 
@@ -8,5 +8,5 @@ import sensible from '@fastify/sensible';
  * @see https://github.com/fastify/fastify-sensible
  */
 export default fp(async function (fastify: FastifyInstance) {
-  fastify.register(sensible);
+	fastify.register(sensible);
 });
