f9047f7a251f6c2cec3beaf56f98a1518210a9a2
Chipzstar <chipzstar.dev@googlemail.com>
2025-12-30 13:44:27 +0000

    feat: Integrates Neon adapter for Prisma
    This commit integrates the Neon adapter for Prisma, enabling connection to Neon's serverless Postgres.
    
    - Updates Prisma to v7 and dependency updates
    - Configures Prisma client to use the Neon adapter with connection string.

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,24 +1,15 @@
-/*import { PrismaClient } from '@prisma/client';
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
-
-export default prisma;*/
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaNeon({ connectionString });
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
+      adapter,
       log:
           process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
     });
